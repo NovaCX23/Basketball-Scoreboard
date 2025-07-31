007e788f6ead4bc7cdec73ff8621d4a1ee0d2b0f
Chris <xhamanichristian@gmail.com>
2025-07-31 15:16:55 +0300

    Basket_Scoreboard

---
+++ index.js
@@ -0,0 +1,33 @@
+const home = document.getElementById("count-home");
+const away = document.getElementById("count-away");
+const resetBtn = document.getElementById("reset");
+
+let homeScore = 0;
+let awayScore = 0;
+
+function updateScore() {
+    home.textContent = homeScore;
+    away.textContent = awayScore;
+}
+
+document.querySelectorAll(".Home .score-btns button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const value = Number(btn.textContent.replace("+", ""));
+        homeScore += value;
+        updateScore();
+    });
+});
+
+document.querySelectorAll(".Away .score-btns button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const value = Number(btn.textContent.replace("+", ""));
+        awayScore += value;
+        updateScore();
+    });
+});
+
+resetBtn.addEventListener("click", () => {
+    homeScore = 0;
+    awayScore = 0;
+    updateScore();
+});
